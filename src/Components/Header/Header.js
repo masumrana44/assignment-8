cf9fb9f0ad93b069a3dab45a0ae080ed02584204
Masum Rana <masumrana446267@gmail.com>
2022-12-19 23:01:39 +0600

    logo & header done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <div className='logo'>
+               <img src='https://th.bing.com/th/id/OIP.d1YWNyBdxmDLANojZ6gFUQHaEc?pid=ImgDet&rs=1' /> 
+               <h1> Exercise-Corner</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
